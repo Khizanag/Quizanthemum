df3cc3dd4cc394754802b4ed48f909c20523d495
Giga <Khizanag@gmail.com>
2020-07-15 21:00:44 +0400

    Innitial commit with default servlets

---
+++ src/main/java/Controller/Servlets/FindServlet.java
@@ -0,0 +1,19 @@
+package Controller.Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "FindServlet", urlPatterns = "find")
+public class FindServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
