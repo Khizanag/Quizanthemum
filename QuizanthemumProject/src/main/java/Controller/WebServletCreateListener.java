9a7d222c4ff0e2d01a1623f95fa44afcfc5c820e
Giga <Khizanag@gmail.com>
2020-07-17 15:35:29 +0400

    Inited webServletCreateListener

--- QuizanthemumProject/src/main/java/Controller/WebServletCreateListener.java
+++ QuizanthemumProject/src/main/java/Controller/WebServletCreateListener.java
@@ -1,6 +1,11 @@
 package Controller;
 
+import Controller.Classes.Quiz.QuizEvent;
 import Model.DatabaseConnector;
+import Model.Managers.ChallengeManager;
+import Model.Managers.QuestionManager;
+import Model.Managers.QuizEventManager;
+import Model.Managers.QuizManager;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -20,7 +25,20 @@ public class WebServletCreateListener implements ServletContextListener, Config 
         context.setAttribute(CONNECTION_STR, connection);
 
         // set challenge manager
+        ChallengeManager challengeManager = new ChallengeManager();
+        context.setAttribute(CHALLENGE_MANAGER_STR, challengeManager);
 
+        // set question manager
+        QuestionManager questionManager = new QuestionManager();
+        context.setAttribute(QUESTION_MANAGER_STR, questionManager);
+
+        // set quiz event manager
+        QuizEventManager quizEventManager = new QuizEventManager();
+        context.setAttribute(QUIZ_EVENT_MANAGER_STR, quizEventManager);
+
+        // set quiz manager
+        QuizManager quizManager = new QuizManager();
+        context.setAttribute(QUIZ_MANAGER_STR, quizManager);
     }
 
     @Override
