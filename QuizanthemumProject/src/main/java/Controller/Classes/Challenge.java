b6ff14f8b6bbc67f5442ce3f91ed5310384441c7
Giga <Khizanag@gmail.com>
2020-07-17 12:33:35 +0400

    Added Manager default classes

--- QuizanthemumProject/src/main/java/Controller/Classes/Challenge.java
+++ QuizanthemumProject/src/main/java/Controller/Classes/Challenge.java
@@ -1,4 +1,20 @@
 package Controller.Classes;
 
+import Controller.Classes.Users.User;
+
+import java.util.Date;
+
 public class Challenge {
+
+    private User challenger;
+    private User challenged;
+    private Date challengingDate;
+
+    public Challenge(User challenger, User challenged, Date challengingDate){
+        this.challenger = challenger;
+        this.challenged = challenged;
+        this.challengingDate = challengingDate;
+    }
+
+
 }
---
+++ QuizanthemumProject/src/main/java/Model/Connector.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.sql.Connection;
+
+public class Connector {
+
+//    private static final Connection connection ;
+
+    private Connector(){
+//        connection
+    }
+
+    public Connection getInstance(){
+        return null;
+    }
+}
