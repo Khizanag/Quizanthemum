ca55d925ebdc69c4d7bf1276ce8f72e88de50da3
ishal17 <ishal17@freeuni.edu.ge>
2020-07-17 13:44:20 +0400

    Question API expanded

--- QuizanthemumProject/src/main/java/Controller/Classes/Quiz/Question.java
+++ QuizanthemumProject/src/main/java/Controller/Classes/Quiz/Question.java
@@ -8,68 +8,91 @@ import java.util.Set;
 
 public class Question {
 
-    private static final String STANDARD = "StandardQuestion";
-    private static final String FILL_BLANK = "FillBlankQuestion";
-    private static final String MULTI_CHOICE = "MultiChoiceQuestion";
-    private static final String MULTI_ANSWER = "MultiAnswerQuestion";
-    private static final String MULTI_CHOICE_MULTI_ANSWER = "MultiChoiceMultiAnswerQuestion";
-    private static final String PICTURE = "PictureQuestion";
+    /* private variables */
 
-    private final String type;
-    private String statement;
-    private int maxScore;
-    private int score;
-
-    private String textAnswer;
-    private String correctTextAnswer;
-
-    private Set<Pair<String>> matchingAnswer;
-    private Set<Pair<String>> correctMatchingAnswer;
-
-    private Set<String> multiAnswer;
-    private Set<String> correctMultiAnswer;
-
-    public Question(String type) {
-        this.type = type;
+    /*
+    question types
+     */
+    public enum Type {
+        STANDARD,
+        FILL_BLANK,
+        MULTI_CHOICE,
+        MULTI_ANSWER,
+        MULTI_CHOICE_MULTI_ANSWER,
+        PICTURE,
+        MATCHING
     }
 
     /*
-     * returns all possible question types
+    type of given question
      */
-    public static List<String> getQuestionTypes() {
-        List<String> types = new ArrayList<String>();
-        types.add(STANDARD);
-        types.add(FILL_BLANK);
-        types.add(MULTI_CHOICE);
-        types.add(MULTI_ANSWER);
-        types.add(PICTURE);
-        types.add(MULTI_CHOICE_MULTI_ANSWER);
-        // TODO add more types
-        return types;
+    private Type type;
+
+    /*
+    maximum score for given question
+     */
+    private int maxScore;
+
+    /*
+    question header statement as text, instruction.
+    for instance: 'match pictures given below to their definitions'
+     */
+    private String headerStatement;
+
+    /*
+    question statement as text
+     */
+    private String textStatement;
+
+    /*
+    multiple choice statement
+     */
+    private List<String> multiChoiceStatement;
+
+    /*
+    multiple answers number
+     */
+    private int multiAnswersNum;
+
+    /*
+    picture statement
+     */
+    private String pictureURL;
+
+    /*
+    matching options left
+     */
+    private List<String> matchingLeft;
+
+    /*
+    matching options right
+     */
+    private List<String> matchingRight;
+
+    /*
+    correct answer for STANDARD, FILL_BLANK, PICTURE and MULTI_CHOICE questions
+     */
+    private String textAnswer;
+
+    /*
+    correct answer for MATCHING questions
+     */
+    private Set<Pair<String>> matchingAnswer;
+
+    /*
+    correct answer for MULTI_ANSWER and MULTI_CHOICE_MULTI_ANSWER questions
+     */
+    private Set<String> multiAnswer;
+
+
+    /* getter and setter methods */
+
+    public Question(Type type) {
+        this.type = type;
     }
 
-    public String getType() {
+    public Type getType() {
         return type;
-    }
-
-    public void setStatement(String statement) {
-        this.statement = statement;
-    }
-
-    public String getStatement() {
-        return statement;
-    }
-
-    public boolean checkAnswer() {
-        return correctTextAnswer.equals(textAnswer);
-    }
-
-    public void setTextAnswer(String answer) {
-        this.textAnswer = answer;
-    }
-
-    public void setCorrectTextAnswer(String correctTextAnswer) {
-        this.correctTextAnswer = correctTextAnswer;
     }
 
     public int getMaxScore() {
@@ -80,11 +103,84 @@ public class Question {
         this.maxScore = maxScore;
     }
 
-    public int getScore() {
-        return score;
+    public String getHeaderStatement() {
+        return headerStatement;
     }
 
-    public void setScore(int score) {
-        this.score = score;
+    public void setHeaderStatement(String headerStatement) {
+        this.headerStatement = headerStatement;
     }
+
+    public String getTextStatement() {
+        return textStatement;
+    }
+
+    public void setTextStatement(String textStatement) {
+        this.textStatement = textStatement;
+    }
+
+    public List<String> getMultiChoiceStatement() {
+        return multiChoiceStatement;
+    }
+
+    public void setMultiChoiceStatement(List<String> multiChoiceStatement) {
+        this.multiChoiceStatement = multiChoiceStatement;
+    }
+
+    public int getMultiAnswersNum() {
+        return multiAnswersNum;
+    }
+
+    public void setMultiAnswersNum(int multiAnswersNum) {
+        this.multiAnswersNum = multiAnswersNum;
+    }
+
+    public String getPictureURL() {
+        return pictureURL;
+    }
+
+    public void setPictureURL(String pictureURL) {
+        this.pictureURL = pictureURL;
+    }
+
+    public List<String> getMatchingLeft() {
+        return matchingLeft;
+    }
+
+    public void setMatchingLeft(List<String> matchingLeft) {
+        this.matchingLeft = matchingLeft;
+    }
+
+    public List<String> getMatchingRight() {
+        return matchingRight;
+    }
+
+    public void setMatchingRight(List<String> matchingRight) {
+        this.matchingRight = matchingRight;
+    }
+
+    public String getTextAnswer() {
+        return textAnswer;
+    }
+
+    public void setTextAnswer(String textAnswer) {
+        this.textAnswer = textAnswer;
+    }
+
+    public Set<Pair<String>> getMatchingAnswer() {
+        return matchingAnswer;
+    }
+
+    public void setMatchingAnswer(Set<Pair<String>> matchingAnswer) {
+        this.matchingAnswer = matchingAnswer;
+    }
+
+    public Set<String> getMultiAnswer() {
+        return multiAnswer;
+    }
+
+    public void setMultiAnswer(Set<String> multiAnswer) {
+        this.multiAnswer = multiAnswer;
+    }
+
 }
--- QuizanthemumProject/src/main/java/Controller/Classes/Quiz/QuestionEvent.java
+++ QuizanthemumProject/src/main/java/Controller/Classes/Quiz/QuestionEvent.java
@@ -1,5 +1,10 @@
 package Controller.Classes.Quiz;
 
 public class QuestionEvent {
-    
+
+    private Question question;
+
+    public QuestionEvent(Question question) {
+        this.question = question;
+    }
 }
