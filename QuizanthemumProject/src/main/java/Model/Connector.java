87c1b5777fe8ad5f28ce586dd23eb1e699b7eafe
Giga <Khizanag@gmail.com>
2020-07-17 13:05:52 +0400

    created Connector

--- QuizanthemumProject/src/main/java/Model/Connector.java
+++ QuizanthemumProject/src/main/java/Model/Connector.java
@@ -1,16 +1,31 @@
 package Model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connector {
 
-//    private static final Connection connection ;
+    public String USERNAME = "root";
+    public String PASSWORD = "paroli12345678";
+    public String URL = "jdbc:mysql://localhost:3306/";
+    public String DATABASE_NAME = "intelij_db";
+    private static Connection connection = null;
 
-    private Connector(){
-//        connection
-    }
+    private Connector(){ }
 
     public Connection getInstance(){
-        return null;
+        if(connection == null)
+            initConnection();
+        return connection;
+    }
+
+    private void initConnection(){
+        System.setProperty("jdbc.drivers", "sun.jdbc.odbc.JdbcodbcDriver");
+        try {
+            connection = DriverManager.getConnection(URL+DATABASE_NAME, USERNAME, PASSWORD);
+        } catch (SQLException unused) {
+            System.out.println("SQLEcxeption: during creating connection in Connector");
+        }
     }
 }
