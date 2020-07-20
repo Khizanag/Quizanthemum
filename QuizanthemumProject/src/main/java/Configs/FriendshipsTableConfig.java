00b31e1a91c06f2497987145d02fb29019a4980f
Giga <Khizanag@gmail.com>
2020-07-20 11:08:17 +0400

    finished FriendshipsTableConfig

---
+++ QuizanthemumProject/src/main/java/Configs/FriendshipsTableConfig.java
@@ -0,0 +1,12 @@
+package Configs;
+
+public interface FriendshipsTableConfig {
+
+    String FRIENDSHIP_TABLE_NAME = "friendship";
+
+    String FRIENDSHIP_TABLE_COLUMN_1_ID = "ID";
+    String FRIENDSHIP_TABLE_COLUMN_2_FIRST_FRIEND_ID = "FIRST_FRIEND_ID";
+    String FRIENDSHIP_TABLE_COLUMN_3_SECOND_FRIEND_ID = "SECOND_FRIEND_ID";
+    String FRIENDSHIP_TABLE_COLUMN_4_MAKE_FRIEND_DATE = "MAKE_FRIEND_DATE";
+    
+}
