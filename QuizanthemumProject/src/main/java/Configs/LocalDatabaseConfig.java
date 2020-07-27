7faf871683344729ed29dbf57bd82d760b4ed270
ZeK1ng <dgogi17@freeuni.edu.ge>
2020-07-28 01:08:23 +0400

    dynamic user info getting

--- QuizanthemumProject/src/main/java/Configs/LocalDatabaseConfig.java
+++ QuizanthemumProject/src/main/java/Configs/LocalDatabaseConfig.java
@@ -1,11 +1,8 @@
 package Configs;
 
 public interface LocalDatabaseConfig {
-
-    /* Database info */
-    String USERNAME = "dima";
-    String PASSWORD = "DimaDima_123";
+    String USERNAME = "root";
+    String PASSWORD = "Pass_word1";
     String URL = "jdbc:mysql://localhost:3306/";
-    String DATABASE_NAME = "demodb";
-
+    String DATABASE_NAME = "quizanthemum_schema";
 }
