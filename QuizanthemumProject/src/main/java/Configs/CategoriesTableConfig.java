32b18ef02d44b3b4759e9a3c8b52be5d280dcfc5
Giga <Khizanag@gmail.com>
2020-08-01 15:16:29 +0400

    finished CategoriesManager

--- QuizanthemumProject/src/main/java/Configs/CategoriesTableConfig.java
+++ QuizanthemumProject/src/main/java/Configs/CategoriesTableConfig.java
@@ -2,6 +2,8 @@ package Configs;
 
 public interface CategoriesTableConfig {
 
+    String CATEGORIES_TABLE_NAME = "categories";
+
     String CATEGORIES_TABLE_COLUMN_1_ID = "ID";
     String CATEGORIES_TABLE_COLUMN_2_NAME = "name";
 }
--- QuizanthemumProject/src/main/java/Model/Managers/CategoriesManager.java
+++ QuizanthemumProject/src/main/java/Model/Managers/CategoriesManager.java
@@ -7,11 +7,13 @@ import Controller.Classes.Quiz.Question.Question;
 import Controller.Classes.Quiz.Question.QuestionEvent;
 import Model.DatabaseConnector;
 
+import javax.xml.crypto.Data;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static Configs.Config.DEFAULT_ID;
 import static Configs.Config.QUESTION_MANAGER_STR;
 
 public class CategoriesManager implements CategoriesTableConfig {
@@ -26,7 +28,38 @@ public class CategoriesManager implements CategoriesTableConfig {
 
     public ManagersManager getManager(){ return this.manager; }
 
-    public void insertCategory(Category category){
+    /* returns ID of category which was added into database */
+    public int insertCategory(Category category){
+        String query = "INSERT INTO " + CATEGORIES_TABLE_NAME
+                + " VALUES (null, " + category.getName() + ")"
+                + ";\n";
+        try{
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            int ID = DatabaseConnector.getLastInsertID();
+            return ID;
+        } catch (SQLException throwables) {
+            System.out.println("Error during creating statement or executing query during inserting Category");
+            throwables.printStackTrace();
+        }
+        return DEFAULT_ID;
+    }
 
+    public Category getCategory(int ID){
+        String query = "SELECT *"
+                + " FROM " + CATEGORIES_TABLE_NAME
+                + " WHERE " + CATEGORIES_TABLE_COLUMN_1_ID + " = " + ID
+                + ";\n";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            if(resultSet.next()){
+                String name = resultSet.getString(CATEGORIES_TABLE_COLUMN_2_NAME);
+                return new Category(ID, name);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 }
