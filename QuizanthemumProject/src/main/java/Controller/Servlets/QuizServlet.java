eb14c30b6f180ad86038456cfc4f09d381858783
Giga <Khizanag@gmail.com>
2020-08-04 04:56:33 +0400

    QuizServlet finished

---
+++ QuizanthemumProject/src/main/java/Controller/Servlets/QuizServlet.java
@@ -0,0 +1,22 @@
+package Controller.Servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "QuizServlet", urlPatterns = "/Quiz")
+public class QuizServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/web/pages/QuizPage.jsp");
+        dispatcher.forward(request, response);
+    }
+}
